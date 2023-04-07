4e5dced4f2d76974b344cbea2eac700141fa9a91
Achour berrahma <berrahmaachour@gmail.com>
2023-04-07 18:07:00 +0200

    consume books backend

---
+++ frontend/src/layouts/Utils/SpinnerLoading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SpinnerLoading() {
+  return (
+    <div className="container mt-5 d-flex justify-content-center " 
+        style={{ height: 550 }}>
+        <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>
+
+  )
+}
+
+export default SpinnerLoading
