1dd15f66d615c81e6f8eb2b3edaf692d08d093c5
Achour berrahma <berrahmaachour@gmail.com>
2023-04-07 16:10:08 +0200

    add backend

---
+++ backend/src/main/java/com/achour/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.achour.springbootlibrary.config;
+
+import com.achour.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        config.exposeIdsFor(Book.class);
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        // Confiure CORS mapping
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        // disable HTTP methods for Book: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
